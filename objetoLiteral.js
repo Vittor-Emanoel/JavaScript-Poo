3e2c2d8d70cb38b90b9300de6c9c0b06891805da
vittor-Emanoel <vittoremanoel1234@gmail.com>
2022-08-11 17:17:41 -0300

    segunda aula

--- objetoLiteral.js
+++ objetoLiteral.js
@@ -2,9 +2,22 @@ const user =  {
     nome: 'Vittor',
     email:  'Vittor@.com',
     nascimento: '21/04/002',
-    role: 'role',
+    role: 'estudante',
     ativo: true,
     exibirInfos: function() {
         console.log(this.nome, this.email);
     }
 }
+
+const admin = {
+    nome: 'Emanoel',
+    email: 'emanoel@.com',
+    role: 'admin',
+    criarCurso: function() {
+      console.log('Curso criado'); //metodo
+    }
+}
+
+Object.setPrototypeOf(admin, user);// o contexto de this é o admin, embora seja um metodo que nn exista dentro de admin
+admin.criarCurso();
+admin.exibirInfos(); // herda
